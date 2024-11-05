41d36a618af644b2eca4075472b85f6af68018d2
s224804 <frederik.bode@hendrichsen.dk>
2024-11-05 11:14:31 +0100

    test.sh automates running all tests

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+test_files=$(find . -name "test_*")
+echo "Running test suite..."
+for file in $test_files; do
+if [[ $file != *.c && $file != *.o ]]; then
+    echo "Running test: $file"
+    ./$file
+  fi
+done
+./aq_test
+echo "All tests completed"
